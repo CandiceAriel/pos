518e9c7585cfd91513332c8b50e8401f54ab81d8
Candice Ariel <candicewangsa@gmail.com>
2023-04-27 13:33:04 +0700

    styling

--- client/src/view/Login.jsx
+++ client/src/view/Login.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='o-login__wrapper'>
-      <div className='o-login__content'>
+    <div className='c-login__wrapper'>
+      <div className='c-login__content'>
         <p>Login</p>
-        <input type='text' className='c-base__input' />
-        <input type='text' className='c-base__input' />
+        <input type='text' className='c-base__input c-base__input-primary' />
+        <input type='text' className='c-base__input c-base__input-primary' />
       </div>
     </div>
   )
